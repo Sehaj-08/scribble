24e13c51a703559b86c24c52b10d1bc707d7969f
Sehaj singh <sehajsingh266c@gmail.com>
2026-09-06 19:26:37 +0530

    Phase 1 Task 3 completed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 const app = express()
 const rooms  = {}  // why Object not ARRAY CAUSE --> Searching iin object is easier no need for looping direct search happend TC is O(1) 
+app.use(express.json())
 app.get("/" , (req,res) => {
     res.send("yo yo")
 })
@@ -24,4 +25,31 @@ app.get("/rooms" , (req,res) => {
         roomId
     }) 
 })  
+
+// a post endpoint where user sends the roomId he wants to enter adn his name 
+app.post("/join_rooms/:room_id" , (req,res) => {
+// check if rooms exists
+    const {room_id} = req.params
+    
+    console.log(room_id)
+    if (!rooms[room_id]){
+        return res.status(400).json("Room no longer exists")
+    }
+    const playerId = Math.random().toString(36).substring(2, 10);
+
+    // storing player isn the player array
+    rooms[room_id].players.push({
+        playerId
+    }) 
+// return all players
+    return res.status(201).json("Player Entered the fucking room")
+
+})
+
+app.get("/get_all_rooms" , (req,res) => {
+    return res.status(200).json({
+        rooms
+    })
+})
+
 export {app}
