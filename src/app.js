112a182f47a0838341ea3f14910786b5c134782b
Sehaj singh <sehajsingh266c@gmail.com>
2026-09-06 18:37:48 +0530

    Phase1 Task 2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,27 @@
 import express from "express"
 const app = express()
-
+const rooms  = {}  // why Object not ARRAY CAUSE --> Searching iin object is easier no need for looping direct search happend TC is O(1) 
+app.get("/" , (req,res) => {
+    res.send("yo yo")
+})
+function generateRoomId(){
+    return Math.random().toString(36).substring(2, 7).toUpperCase();
+}
+app.get("/rooms" , (req,res) => {
+    //for storing multiple rooms create a js object   
+    // for current room making generate a unique id
+    const roomId = generateRoomId()
+    // check if the id exists for enyother room
+    while(rooms[roomId]){
+        roomId = generateRoomId()
+    } 
+    // create playes for that rooom
+    rooms[roomId] = {
+        players : []
+    } 
+    //return room id
+    return res.status(201).json({
+        roomId
+    }) 
+})  
 export {app}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-import {app} from "./app.js"
 import "dotenv/config"
+import {app} from "./app.js"
 
 const port = process.env.PORT || 8000
 
-app.get("/" , (req,res) => {
-    res.send("yo yo")
-})
+
 app.listen(port , () => {
     console.log("Fuck you bitch")
 })
